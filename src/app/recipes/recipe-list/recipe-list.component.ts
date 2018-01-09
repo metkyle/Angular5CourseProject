3af6a540abf644d381c029f0553658f1d0fb93ac
metkyle <kyle.metcalfe@eagles.ewu.edu>
2018-01-08 21:54:55 -0800

    Adding files to repository

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipes } from '../recipes.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipes[] = [
+    new Recipes('A Test Recipes', 'This is simply a test', 
+    'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=KPTNrvis')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
